a15987b32a7e290acced5a6cbe4b5fc8cee87215
jinhee <leejinhee3456@gmail.com>
2022-12-15 03:47:57 +0900

    Feat: msw 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const isServer = !process.browser;
+
+  if (process.env.NODE_ENV === 'development') {
+    if (isServer) {
+      (async () => {
+        const { server } = await import('../src/mocks/server');
+        server.listen();
+      })();
+    } else {
+      (async () => {
+        const { worker } = await import('../src/mocks/browser');
+        worker.start();
+      })();
+    }
+  }
+
+  return <Component {...pageProps} />;
 }
