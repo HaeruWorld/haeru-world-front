5a809740834e250bbcfea2cf90e6752e2e96df7c
jinhee <leejinhee3456@gmail.com>
2022-12-15 02:15:21 +0900

    Feat: 기본 환경 세팅
    1. eslint 설정
    2. prettier 설정
    3. 절대경로 설정

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack(config) {
+    config.resolve.alias = {
+   ...config.resolve.alias,
+        '@': path.resolve(__dirname, 'src'),
+  };
+
+    return config;
+  },
 }
 
 module.exports = nextConfig
