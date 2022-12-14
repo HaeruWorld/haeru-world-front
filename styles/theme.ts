2bf25eae823227e29ae74414d19ec3e86cba596e
jinhee <leejinhee3456@gmail.com>
2022-12-15 03:17:51 +0900

    Feat: Prettier, styled-components 패키지 설치 및 테마 설정
    styled-components SSR 설정

---
+++ styles/theme.ts
@@ -0,0 +1,20 @@
+const theme = {
+  color: {
+    primary_01: '#0E235A',
+    primary_02: '#425584',
+    primary_03: '#B5D2ED',
+    primary_04: '#E8F2FB',
+    sub_01: '#FF8845',
+    sub_02: '#FFB58B',
+    sub_03: '#FFDECC',
+    gray_01: '#1D1E20',
+    gray_02: '#4C4F56',
+    gray_03: '#6A6F79',
+    gray_04: '#C6C9D0',
+    gray_05: '#E7E9EC',
+    gray_06: '#F6F7F9',
+    gray_07: '#FFFFFF',
+  },
+};
+
+export default theme;
