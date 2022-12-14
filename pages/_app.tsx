c279042bdba13cc18bb1ce8dea17e0c22039f67e
jinhee <leejinhee3456@gmail.com>
2022-12-15 03:54:12 +0900

    Feat: ThemeProvider 래핑
      본문(추가 설명)을 아랫줄에 작성

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
+import theme from 'styles/theme';
 
 export default function App({ Component, pageProps }: AppProps) {
   const isServer = !process.browser;
@@ -18,5 +20,9 @@ export default function App({ Component, pageProps }: AppProps) {
     }
   }
 
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
