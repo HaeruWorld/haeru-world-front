f34b8fc706a131e73a8de3076934211e833e24c1
jinhee <leejinhee3456@gmail.com>
2022-12-15 22:42:07 +0900

    Feat: Mock Handler 및 QueryKey 생성 유틸성 함수 구현

--- src/utils/queries/getQueryKeys.ts
+++ src/utils/queries/getQueryKeys.ts
@@ -1,4 +1,7 @@
-type QueryList = {};
+type QueryList = {
+  // 해루질 가능한 장소 상세 페이지
+  HAERU_PLACES_DETAIL: number;
+};
 
 const getQueryKey = <T extends keyof QueryList>(
   ...[key, params]: undefined extends QueryList[T] ? [T] : [T, QueryList[T]]
