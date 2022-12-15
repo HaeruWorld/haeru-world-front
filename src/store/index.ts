1e84980c21dc0632b3f54aa846b35bbc7cfef0fd
jinhee <leejinhee3456@gmail.com>
2022-12-15 22:49:41 +0900

    Feat: Recoil atom 설정

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,1 +1,9 @@
-export {};
+import { atom } from 'recoil';
+
+// 선택한 지역 상태
+const areaAtom = atom({ key: 'area', default: null });
+
+// 선택한 어종 리스트
+const marineCollectionsAtom = atom({ key: 'marineCollections', default: [] });
+
+export { areaAtom, marineCollectionsAtom };
