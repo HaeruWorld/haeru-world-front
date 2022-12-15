55b531f5b219997b2064cfc4a52c80e689c160b4
jinhee <leejinhee3456@gmail.com>
2022-12-15 14:21:35 +0900

    Feat: 카카오 맵 기본 세팅

--- pages/haeruPlaces/[id].tsx
+++ pages/haeruPlaces/[id].tsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import { Map, MapMarker } from 'react-kakao-maps-sdk';
 
-const HaeruPlaceDetail = () => <div>hello world</div>;
+const HaeruPlaceDetail = () => {
+  return (
+    <Map
+      center={{ lat: 33.5563, lng: 126.79581 }}
+      style={{ width: '100%', height: '360px' }}
+    >
+      <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
+        <div style={{ color: '#000' }}>Hello World!</div>
+      </MapMarker>
+    </Map>
+  );
+};
 
 export default HaeruPlaceDetail;
