59a83965ca8563a03d313c2d98ac5237f98f84d0
Dongmin Ahn <eamon3481@naver.com>
2022-12-16 00:47:53 +0900

    Chore : next config img domain 추가

--- next.config.js
+++ next.config.js
@@ -1,17 +1,21 @@
 const path = require('path');
-
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   webpack(config) {
     config.resolve.alias = {
-   ...config.resolve.alias,
-        '@': path.resolve(__dirname, 'src'),
-  };
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, 'src'),
+    };
 
     return config;
   },
-}
 
-module.exports = nextConfig
+  images: {
+    domains: ['https://haeruworld.run.goorm.io'],
+  },
+};
+
+module.exports = nextConfig;
