c5dbdde3dfe676570afc36064600f8054000d914
jinhee <leejinhee3456@gmail.com>
2022-12-15 23:03:45 +0900

    Fix: 해루 장소 상세 api response 형태 변경
    dehydrate 시, AxiosResponse 객체 형태를 props로 넘길 수 있게 data만 뽑아서 수정

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -3,7 +3,7 @@ import superFetch from './superFetch';
 const api = {
   // 해루질 할 수 있는 장소 상세 페이지 요청 API
   getHaeruPlaceDetail: async (id: number) => {
-    return superFetch.get(`/haeruPlaces/${id}`);
+    return superFetch.get(`/haeruPlaces/${id}`).then((res) => res.data);
   },
 };
 
