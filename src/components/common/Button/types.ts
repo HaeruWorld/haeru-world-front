3297dd97270e5830cb092e21071661d618f51b79
jinhee <leejinhee3456@gmail.com>
2022-12-16 00:29:37 +0900

    Feat: Button 컴포넌트 Type 파일 생성

---
+++ src/components/common/Button/types.ts
@@ -0,0 +1,11 @@
+import { MouseEventHandler } from 'react';
+
+export type ButtonType = {
+  className?: string;
+  text: string;
+  radius?: number;
+  color?: string;
+  disabled?: boolean;
+  backgroundColor: string;
+  onClick?: MouseEventHandler;
+};
