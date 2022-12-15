63a6eb609224ce07748d96a2bcdcbe0703b68bb9
Dongmin Ahn <eamon3481@naver.com>
2022-12-15 21:03:46 +0900

    Style : MapIllustration 레이아웃 추가

---
+++ src/components/haeruPlaces/mapIllustration/style.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const MapIllustrationStyle = styled.div`
+  position: relative;
+  width: 335px;
+  height: 220px;
+  background: #f4f4f4;
+  border-radius: 8px;
+`;
